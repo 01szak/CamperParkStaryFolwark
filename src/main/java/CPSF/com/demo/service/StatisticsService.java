e4a5ae92046bef780f878bb677a18ab5144aadcf
01szak <kacperolszewski2006@gmail.com>
2025-02-27 16:49:40 +0100

    added StatisticService and StatisticsController

---
+++ src/main/java/CPSF/com/demo/service/StatisticsService.java
@@ -0,0 +1,37 @@
+package CPSF.com.demo.service;
+
+import CPSF.com.demo.entity.Reservation;
+import CPSF.com.demo.repository.ReservationRepository;
+import lombok.AllArgsConstructor;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@AllArgsConstructor
+@RequiredArgsConstructor
+public class StatisticsService {
+
+    @Autowired
+    ReservationRepository reservationRepository;
+
+    public int reservationCount(int camperPlaceId, int month, int year) {
+        if (month == 0) {
+            return reservationRepository.findReservationByCamperPlace_IdAndAndCheckin_Year(camperPlaceId, year).size();
+
+        }
+        return reservationRepository.findReservationByCamperPlace_IdAndCheckin_MonthAndAndCheckin_Year(camperPlaceId, month, year).size();
+    }
+
+    public double revenueCount(int camperPlaceId, int month, int year) {
+        if (month == 0) {
+            List<Reservation> reservations = reservationRepository.findReservationByCamperPlace_IdAndAndCheckin_Year(camperPlaceId, year);
+            return reservations.stream().mapToDouble(Reservation::calculateFinalPrice).sum();
+        }
+        List<Reservation> reservations = reservationRepository.findReservationByCamperPlace_IdAndCheckin_MonthAndAndCheckin_Year(camperPlaceId, month, year);
+        return reservations.stream().mapToDouble(Reservation::calculateFinalPrice).sum();
+    }
+
+}
