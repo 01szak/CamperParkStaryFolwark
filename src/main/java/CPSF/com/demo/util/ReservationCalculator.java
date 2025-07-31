f9201d45edff3038415d907a1b7a1d573f00bddb
01szak <kacperolszewski2006@gmail.com>
2025-07-31 22:47:21 +0200

    price calc adjustment p3

--- src/main/java/CPSF/com/demo/util/ReservationCalculator.java
+++ src/main/java/CPSF/com/demo/util/ReservationCalculator.java
@@ -8,13 +8,13 @@ import java.time.LocalDate;
 public class ReservationCalculator {
 
 	private static final long discount = 10;
-	private static final long daysToDiscount = 3;
+	private static final long daysToDiscount = 4;
 
 
 	public double calculateFinalReservationCost(long daysInrReservation, double camperPlacePrice) {
 		long finalCost = 0;
 		for (int i = 1; i <= daysInrReservation; i++ ) {
-			if (i >= daysToDiscount) {
+			if (i == daysToDiscount) {
 				camperPlacePrice -= discount;
 			}
 			finalCost += (long) camperPlacePrice;
