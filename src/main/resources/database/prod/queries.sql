e1f6b73f0c87ae81d5414ee39dd648149f941575
01szak <kacperolszewski2006@gmail.com>
2025-09-06 14:25:39 +0200

    added new columns

--- src/main/resources/database/prod/queries.sql
+++ src/main/resources/database/prod/queries.sql
@@ -65,3 +65,35 @@ ALTER TABLE camper_places ADD COLUMN updated_at DATE;
 
 ALTER TABLE reservations ADD COLUMN created_at DATE;
 ALTER TABLE reservations ADD COLUMN updated_at DATE;
+
+-- changeset kacper:7
+
+ALTER TABLE users
+DROP COLUMN created_at;
+ALTER TABLE users
+DROP COLUMN updated_at;
+
+ALTER TABLE camper_places
+DROP COLUMN created_at;
+ALTER TABLE camper_places
+DROP COLUMN updated_at;
+
+ALTER TABLE reservations
+DROP COLUMN created_at;
+ALTER TABLE reservations
+DROP COLUMN updated_at;
+
+ALTER TABLE users
+    ADD COLUMN created_at DATETIME;
+ALTER TABLE users
+    ADD COLUMN updated_at DATETIME;
+
+ALTER TABLE camper_places
+    ADD COLUMN created_at DATETIME;
+ALTER TABLE camper_places
+    ADD COLUMN updated_at DATETIME;
+
+ALTER TABLE reservations
+    ADD COLUMN created_at DATETIME;
+ALTER TABLE reservations
+    ADD COLUMN updated_at DATETIME;
