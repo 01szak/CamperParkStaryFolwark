4750d9f524d16fbd82bcfba93dcb85ad42c434e5
01szak <kacperolszewski2006@gmail.com>
2026-02-15 17:18:15 +0100

    bugfix - camperPlace should not be sorted by updatedAt by default

--- src/main/java/CPSF/com/demo/repository/CamperPlaceRepository.java
+++ src/main/java/CPSF/com/demo/repository/CamperPlaceRepository.java
@@ -1,10 +1,18 @@
 package CPSF.com.demo.repository;
 
 import CPSF.com.demo.model.entity.CamperPlace;
-import org.springframework.data.jpa.repository.EntityGraph;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface CamperPlaceRepository extends CRUDRepository<CamperPlace> {
+
+    @Query("""
+        select c from CamperPlace c order by cast(c.index as integer ), c.index
+    """)
+    List<CamperPlace> findAllOrderByIndex();
+
 
 }
