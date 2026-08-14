1d2feaa0ce18958551575c4329900b6c9583fdab
kacper <kacperolszewski2006@gmail.com>
2026-08-14 15:32:18 +0200

    enable jwt token authentication when web app headers are no present / implement caching for web app auth

--- src/main/java/CPSF/com/demo/configuration/WebAppAuthFilter.java
+++ src/main/java/CPSF/com/demo/configuration/WebAppAuthFilter.java
@@ -2,15 +2,18 @@ package CPSF.com.demo.configuration;
 
 import CPSF.com.demo.exception.AuthorizationException;
 import CPSF.com.demo.model.constant.Operation;
+import CPSF.com.demo.model.entity.User;
 import CPSF.com.demo.service.core.OrganisationService;
 import CPSF.com.demo.service.core.SearchCriteria;
 import CPSF.com.demo.service.core.UserService;
+import com.github.benmanes.caffeine.cache.Cache;
+import com.github.benmanes.caffeine.cache.Caffeine;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
-import org.springframework.security.authentication.AuthenticationManager;
+import lombok.extern.log4j.Log4j2;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.context.SecurityContextHolder;
@@ -21,7 +24,9 @@ import java.util.List;
 import java.util.Optional;
 
 import static CPSF.com.demo.model.constant.UserRole.WEB_APP;
+import static java.util.concurrent.TimeUnit.MINUTES;
 
+@Log4j2
 @RequiredArgsConstructor
 public class WebAppAuthFilter extends OncePerRequestFilter {
 
@@ -30,6 +35,11 @@ public class WebAppAuthFilter extends OncePerRequestFilter {
 
     private final OrganisationService organisationService;
     private final UserService userService;
+    private final Cache<String, User> authCache = Caffeine.newBuilder()
+            .expireAfterWrite(5, MINUTES)
+            .maximumSize(1)
+            .build();
+
 
     @Override
     protected void doFilterInternal(
@@ -39,35 +49,54 @@ public class WebAppAuthFilter extends OncePerRequestFilter {
     ) throws ServletException, IOException {
         var orgId = request.getHeader(ORGANISATION_ID_HEADER);
         var apiKey = request.getHeader(API_KEY_HEADER);
+        var headersPresent = orgId != null && apiKey != null;
 
-        Optional.ofNullable(apiKey).ifPresentOrElse(_ -> {
-            var organisation = organisationService.findById(Integer.parseInt(orgId));
-            var compareApiKey = organisation.getWebAppApiKey();
+        if (headersPresent) {
+            var appUser = authCache.getIfPresent(getCacheKey(orgId, apiKey));
 
-            var appUser = userService.findBy(
-                    new SearchCriteria("organisation", "id", Operation.EQUALS, orgId),
-                    new SearchCriteria("userRole", Operation.EQUALS, "WEB_APP")
-            )
-            .stream()
-            .findFirst()
-            .orElseThrow(() -> new AuthorizationException("No web app associated for the given organisation"));
-
-            if (compareApiKey.equals(apiKey)) {
-                var auth = new UsernamePasswordAuthenticationToken(
-                        appUser.getLogin(),
-                        null,
-                        List.of(new SimpleGrantedAuthority(WEB_APP.getAuthority()))
-                );
-
-                SecurityContextHolder.getContext().setAuthentication(auth);
-            } else {
-                throw new AuthorizationException("Api key not matched");
-            }
-        }, () -> {
-            throw new AuthorizationException("Api key not found");
-        });
+            Optional.ofNullable(appUser).ifPresentOrElse(
+                    this::authoriseUser,
+                    () -> authenticateUser(orgId, apiKey)
+            );
+        }
 
         filterChain.doFilter(request, response);
+    }
+
+    private void authenticateUser(String orgId, String apiKey) {
+        logger.info("Web app auth headers found, starting Authentication for orgId: " + orgId);
+
+        var organisation = organisationService.findById(Integer.parseInt(orgId));
+        var compareApiKey = organisation.getWebAppApiKey();
+        var appUser = userService.findBy(
+                        new SearchCriteria("organisation", "id", Operation.EQUALS, orgId),
+                        new SearchCriteria("userRole", Operation.EQUALS, "WEB_APP")
+                )
+                .stream()
+                .findFirst()
+                .orElseThrow(() -> new AuthorizationException("No web app associated for the given organisation"));
+
+        if (apiKey.equals(compareApiKey)) {
+            authoriseUser(appUser);
+            logger.info("Web app authentication succeeded for orgId: " + orgId);
+        } else {
+            throw new AuthorizationException("Api key not matched");
+        }
+        authCache.put(getCacheKey(orgId, apiKey), appUser);
+
+    }
+
+    private void authoriseUser(User appUser) {
+        var auth = new UsernamePasswordAuthenticationToken(
+                appUser.getLogin(),
+                null,
+                List.of(new SimpleGrantedAuthority(WEB_APP.getAuthority()))
+        );
+        SecurityContextHolder.getContext().setAuthentication(auth);
+    }
+
+    private static String getCacheKey(String orgId, String apiKey) {
+        return orgId + ":" + apiKey;
     }
 
 }
