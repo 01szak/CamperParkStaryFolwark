d78c520fa534f4f6ac8696ca9da63b4b8177a8c7
kacper <kacperolszewski2006@gmail.com>
2026-08-14 15:41:56 +0200

    swagger fix

--- src/main/java/CPSF/com/demo/configuration/SwaggerConfiguration.java
+++ src/main/java/CPSF/com/demo/configuration/SwaggerConfiguration.java
@@ -11,17 +11,23 @@ import org.springframework.stereotype.Component;
 @Component
 public class SwaggerConfiguration {
 
+    private static final String SEC_SCHEME_NAME = "ParceoSecurityScheme";
+    private static final String SEC_SCHEME = "bearer";
+    private static final String BEARER_FORMAT = "JWT";
+    private static final String INFO_TITTLE = "Parceo api scheme";
+
     @Bean
     public OpenAPI customOpenApi() {
-        return  new OpenAPI().
-                info(new Info().title("Parceo api scheme"))
-                .addSecurityItem(new SecurityRequirement().addList("ParceoSecurityScheme"))
-                .components(new Components().addSecuritySchemes("", new SecurityScheme()
-                        .name("ParceoSecurityScheme")
-                        .type(SecurityScheme.Type.HTTP)
-                        .scheme("bearer")
-                        .bearerFormat("JWT"))
-                );
+        var secScheme = new SecurityScheme()
+                .name(SEC_SCHEME_NAME)
+                .scheme(SEC_SCHEME)
+                .bearerFormat(BEARER_FORMAT)
+                .type(SecurityScheme.Type.HTTP);
+        var components = new Components().addSecuritySchemes(SEC_SCHEME_NAME, secScheme);
 
+        return new OpenAPI()
+                .info(new Info().title(INFO_TITTLE))
+                .addSecurityItem(new SecurityRequirement().addList(SEC_SCHEME_NAME))
+                .components(components);
     }
 }
