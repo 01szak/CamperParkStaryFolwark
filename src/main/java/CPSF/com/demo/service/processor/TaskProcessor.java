cbb2fd5eb1859b68ca2492b98806128c6cbbe5ba
kacper <kacperolszewski2006@gmail.com>
2026-08-21 01:13:34 +0200

    store tasks as a tree of sub tasks to run them sequentially

--- src/main/java/CPSF/com/demo/service/processor/TaskProcessor.java
+++ src/main/java/CPSF/com/demo/service/processor/TaskProcessor.java
@@ -12,8 +12,13 @@ import lombok.extern.log4j.Log4j2;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.Optional;
 import java.util.concurrent.Executors;
+import java.util.stream.Gatherer;
 
 import static CPSF.com.demo.model.constant.TaskStatus.EXECUTED;
 import static CPSF.com.demo.model.constant.TaskStatus.FAILED;
@@ -37,6 +42,8 @@ public class TaskProcessor {
                         new SearchCriteria("retryable", Operation.EQUALS, "true", JoinOperator.AND)
                         ).get().toList();
 
+        final var taskForest = pendingTasks.stream().gather(toTaskForest()).toList();
+
         if (pendingTasks.isEmpty()) {
             return;
         }
@@ -44,19 +51,8 @@ public class TaskProcessor {
         increaseRetryCount(pendingTasks);
         try(final var executor =  Executors.newVirtualThreadPerTaskExecutor()) {
             log.info("{} pending tasks found", pendingTasks.size());
-            pendingTasks.forEach(t -> {
-                executor.submit(()-> {
-                    try {
-                        log.info("Starting to process task: {} ", t.getTaskType());
-                        taskService.update(mapTaskStatus(t, IN_PROGRESS));
-                        executableTaskFactory.getExecutableTask(t).doTask();
-                        taskService.update(mapTaskStatus(t, EXECUTED));
-                        log.info("task: {} executed successfully", t.getTaskType());
-                    } catch (Exception e) {
-                        log.error("Exception occurred while processing the task: {} {}", t.getTaskType(), e);
-                        taskService.update(mapTaskStatus(t, FAILED));
-                    }
-                });
+            taskForest.forEach(node -> {
+                executor.submit(() -> executeTaskNode(node));
             });
         } catch (Exception e) {
             log.error("Exception occurred while processing the tasks: {}", e.getLocalizedMessage());
@@ -64,6 +60,29 @@ public class TaskProcessor {
                     (List<Task>) taskService.findBy(new SearchCriteria("taskStatus" , Operation.EQUALS, IN_PROGRESS.toString())).get().toList();
             taskService.update(inProgressTasks.stream().map(t -> mapTaskStatus(t, FAILED)).toList());
         }
+    }
+
+    private void executeTaskNode(TaskNode taskNode) {
+        try {
+            log.info("Starting to process task: {} ", taskNode.task().getTaskType());
+            taskService.update(mapTaskStatus(taskNode.task(), IN_PROGRESS));
+            executableTaskFactory.getExecutableTask(taskNode.task()).doTask();
+            taskService.update(mapTaskStatus(taskNode.task(), EXECUTED));
+            log.info("task: {} executed successfully", taskNode.task().getTaskType());
+        } catch (Exception e) {
+            log.error("Exception occurred while processing the task: {} {}", taskNode.task().getTaskType(), e);
+            taskService.update(mapTaskStatus(taskNode.task(), FAILED));
+            failDescendants(taskNode);
+        }
+    }
+
+    private void failDescendants(TaskNode taskNode) {
+        taskNode.subTasks().forEach(s -> {
+            log.warn("Skipping child task {} with ID{} because parent task failed", s.task().getTaskType(), s.task.getId());
+            taskService.update(mapTaskStatus(s.task(), FAILED));
+            failDescendants(s);
+
+        });
     }
 
     private void increaseRetryCount(List<Task> pendingTasks) {
@@ -75,5 +94,46 @@ public class TaskProcessor {
     private <T extends Task> T mapTaskStatus(T task, TaskStatus taskStatus) {
         task.setTaskStatus(taskStatus);
         return task ;
+    }
+
+    private record TaskNode(Task task, List<TaskNode> subTasks) {
+
+        public TaskNode(Task task) {
+            this(task, new ArrayList<>());
+        }
+
+        public void addSubTask(Task task) {
+            subTasks().add(new TaskNode(task, new ArrayList<>()));
+        }
+    }
+
+    private static Gatherer<Task, Map<Integer, TaskNode>, TaskNode> toTaskForest() {
+        return Gatherer.ofSequential(
+                HashMap::new,
+                (map, task, downstream) -> {
+                    map.put(task.getId(), new TaskNode(task));
+                    return true;
+                },
+                (map, downstream) -> {
+                    final var roots = new ArrayList<>();
+
+                    map.values().forEach(v -> {
+                        final var parent = v.task().getParentTask();
+
+                        Optional.ofNullable(parent).ifPresentOrElse(t -> {
+                            if (map.containsKey(parent.getId())) {
+                                map.get(parent.getId()).addSubTask(v.task());
+                            }
+                        }, () -> {
+                            roots.add(v.task());
+                        });
+
+                    });
+
+                    roots.forEach(root -> {
+                        if (!downstream.push((TaskNode) root)) return;
+                    });
+                }
+        );
     }
 }
