6a38c09c8a98fb5f1459f1dae348cf8707718c09
kacper <kacperolszewski2006@gmail.com>
2026-09-07 19:56:01 +0200

    restore missing mig files and include new one in liquibase-changelog

--- src/main/resources/database/mig/08-08-2026/create_organisation_dev.sql
+++
@@ -1,6 +0,0 @@
--- liquibase formatted sql
--- changeset 01szak:create_organisation_dev.sql
--- validCheckSum: ANY
--- preconditions onFail:MARK_RAN
--- precondition-sql-check expectedResult:1 SELECT COUNT(*) FROM information_schema.tables WHERE table_schema = DATABASE() AND table_name = 'organisation'
-INSERT INTO organisation(owner_id, organisation_name, address, created_at, updated_at, web_app_api_key) VALUES (1, 'Camperpark Stary Folwark', 'Stary Folwark 12', NOW(), NOW(), '123abc')
